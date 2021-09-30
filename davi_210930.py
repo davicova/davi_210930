05e8bd2b61c36ff414b9778a6e83f8c06b037d55
davicova <86981409+davicova@users.noreply.github.com>
2021-09-30 00:17:05 -0300

    Add files via upload

---
+++ davi_210930.py
@@ -0,0 +1,13 @@
+print("Question 2 ************************")
+v=True
+print("Question 3 ************************")
+for item in range(10,21):
+    if item < 14:
+        v=False
+print("Question 4 ************************")
+if v:
+    print("nenhum numero maior que 14")
+
+else:
+
+    print("um numero maior que 14")
